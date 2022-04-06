2bb2452301603b8dba52023531a08346df7d68b5
Capco707 <1298720064@qq.com>
2022-04-06 19:54:58 +0800

    scene

---
+++ Assets/ml-imagesynthesis/ImageSynthesis/GetPerception.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents.Sensors;
+using UnityEngine;
+
+public class GetPerception : MonoBehaviour
+{
+    private CameraSensorComponent[] testSensors;
+
+    private enum cameraRecord
+    {
+        img = 0,
+        id = 1,
+        layer = 2,
+        depth = 3,
+        tag = 6,
+    }
+
+    public Camera mainCamera;
+    private GetRenderTexture RenderTextureScripts;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        RenderTextureScripts = mainCamera.GetComponent<GetRenderTexture>();
+        testSensors = gameObject.GetComponents<CameraSensorComponent>();
+
+        testSensors[0].Camera = RenderTextureScripts.capturePasses[(int)cameraRecord.img].camera;
+        testSensors[1].Camera = RenderTextureScripts.capturePasses[(int)cameraRecord.tag].camera;
+        testSensors[2].Camera = RenderTextureScripts.capturePasses[(int)cameraRecord.depth].camera;
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int count = 0; count < testSensors.Length; count ++)
+        {
+            //Debug.Log(testSensors[count].Camera);
+        }
+    }
+}
